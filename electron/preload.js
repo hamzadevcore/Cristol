19f3ba328eed0f9cfa5d008bd86624719ac5178d
hamzadevcore <hamza.devcore@gmail.com>
2026-03-03 21:05:25 +0300

    Create preload.js

---
+++ electron/preload.js
@@ -0,0 +1,11 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  minimize: () => ipcRenderer.invoke('window-minimize'),
+  maximize: () => ipcRenderer.invoke('window-maximize'),
+  close: () => ipcRenderer.invoke('window-close'),
+  isMaximized: () => ipcRenderer.invoke('window-is-maximized'),
+  onWindowStateChanged: (callback) => {
+    ipcRenderer.on('window-state-changed', (_event, state) => callback(state));
+  }
+});
